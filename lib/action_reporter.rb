14a6f0e1cc06a244eb6b35d276e69136bb12dc48
Andrei Makarov <andrei@kiskolabs.com>
2023-08-30 09:41:58 +0300

    Set ruby requirement to 2.5.0, add codecov integration, update release script

--- lib/action_reporter.rb
+++ lib/action_reporter.rb
@@ -9,7 +9,7 @@ require 'action_reporter/audited_reporter'
 module ActionReporter
   module_function
 
-  VERSION = '1.3.2'
+  VERSION = '1.4.0'.freeze
 
   AVAILABLE_REPORTERS = [
     ActionReporter::RailsReporter,
