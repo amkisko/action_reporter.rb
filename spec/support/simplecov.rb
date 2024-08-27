dc2ebb22da0f2c7831aad140c37f2f855d4ccff7
Andrei Makarov <andrei@kiskolabs.com>
2024-08-27 12:43:29 +0300

    Update formatting

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,9 +1,9 @@
-require 'simplecov'
+require "simplecov"
 
 SimpleCov.start do
-  add_filter '/spec/'
+  add_filter "/spec/"
   add_filter { |source_file| source_file.lines.count < 5 }
 end
 
-require 'simplecov-cobertura'
+require "simplecov-cobertura"
 SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
