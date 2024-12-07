05c9721bd719b01c1ce385ed5bc9f2960f350df7
Andrei Makarov <andrei@kiskolabs.com>
2024-12-07 08:49:49 +0200

    Update test workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,18 @@
-require_relative "../lib/action_reporter"
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter { |source_file| source_file.lines.count < 5 }
+end
+
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 require "webmock/rspec"
 
-RSpec.shared_context "ActionReporter" do
+require "action_reporter"
+
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require_relative f }
+
+RSpec.configure do |config|
+
 end
--- spec/support/simplecov.rb
+++
@@ -1,9 +0,0 @@
-require "simplecov"
-
-SimpleCov.start do
-  add_filter "/spec/"
-  add_filter { |source_file| source_file.lines.count < 5 }
-end
-
-require "simplecov-cobertura"
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
