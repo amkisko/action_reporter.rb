9860936d91f2618d0f8668ee84fb622e464caecf
Andrei Makarov <andrei@kiskolabs.com>
2025-11-10 09:47:19 +0200

    Require logger for Ruby 3.1+ compatibility in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ end
 require "simplecov-cobertura"
 SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
+# Require logger for Ruby 3.1+ compatibility (Logger is no longer in stdlib)
+require "logger"
+
 require "action_reporter"
 
 Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require_relative f }
